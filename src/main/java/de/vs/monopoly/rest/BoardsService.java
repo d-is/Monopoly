9651f53f8ba84341b77e8486a036e9dc79ac8faa
magnus <magnus@philipp>
2015-12-08 16:55:47 +0100

    neu

--- src/main/java/de/vs/monopoly/rest/BoardsService.java
+++ src/main/java/de/vs/monopoly/rest/BoardsService.java
@@ -1,36 +1,20 @@
 package de.vs.monopoly.rest;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
 
-import java.io.IOException;
+import com.google.gson.Gson;
 
-import com.google.gson.*;
-
-import de.vs.monopoly.model.*;
-import retrofit.ClientInterface;
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import spark.Route;
+import de.vs.monopoly.model.Board;
 import de.vs.monopoly.model.Game;
 import de.vs.monopoly.model.Player;
-import de.vs.monopoly.model.*;
-import retrofit.ClientInterface;
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import spark.Route;
-import de.vs.monopoly.model.*;
-import retrofit.ClientInterface;
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import spark.Route;
+import de.vs.monopoly.model.Roll;
 
 public class BoardsService {
 	public static void main(String[] args) {
 		Game game = new Game();
 
-		Dice dice = new Dice();
 		Board board = new Board();
-		Player player;
 
 		Gson gson = new Gson();
 
@@ -44,6 +28,7 @@ public class BoardsService {
 		// Position des Spielers abfragen
 		get("/boards/:gameid/players/:playerid", (request, response) -> {
 			String json = null;
+			//eventuell an gameservice weiterleiten
 			for (Player elem : game.getPlayers()) {
 				if (elem.getId().equals(request.params(":playerid"))) {
 					json = gson.toJson(elem.getPosition());
@@ -54,51 +39,22 @@ public class BoardsService {
 			return json;
 
 		});
-		// Position des Spielers ändern
-		put("/boards/:gameid/players/:playerid/:position", (request, response) -> {
-			String json = null;
+
+		//client übergibt würfelwurf
+		post("/boards/:gameid/players:playerid/roll", (request, response) -> {
+			Roll roll = gson.fromJson(request.body(), Roll.class);
+			roll.getNumber();
 			for (Player elem : game.getPlayers()) {
 				if (elem.getId().equals(request.params(":playerid"))) {
-					elem.setPosition(Integer.valueOf(request.params(":position")));
+					elem.setPosition(roll.getNumber());
 					break;
 				}
 			}
-
-			return null;
-		});
-
-		//
-		post("/boards/:gameid/players:playerid/roll", (request, response) -> {
-			dice.wuerfeln();
-			String json = gson.toJson(dice.getRoll());
-			response.status(200);
-			return json;
+			return true;
 		});
 
 	}
 
 }
 
-// get("/dice", (request, response) -> {
-// dice.wuerfeln();
-// String json = gson.toJson(dice.getRoll());
-// response.status(200);
-// return json;
-// });
-// get("/dice", (request, response) -> {
-// dice.wuerfeln();
-// String json = gson.toJson(dice.getRoll());
-// response.status(200);
-// return json;
-// });
-// ///{gameid}/players/{playerid}/roll
-//
-//
-//
-//
-// get("/games/:gameid/players", (request, response) ->{
-// for(Game game : gameListe){
-// if(game.equals(request.params(":gameid"))){
-// response.status(200);
-// return game.getPlayers();
-// }}response.status(400);return"Gameid wrong.";});
+
