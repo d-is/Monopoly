1e9a534350116dc644b2b58c9f19c94b6a3f203a
Danton <Danton@WIN-6TUP2KB8N50.localdomain>
2016-01-06 16:03:01 +0100

    Noch eine Config Klasse angelegt... wo alle URLs und die sonstigens URIs wie boards und co drin verwaltet werden

---
+++ src/main/java/Config.java
@@ -0,0 +1,8 @@
+
+public class Config {
+ public static final String baseURL = "http://localhost";
+ public static final String verzeichnisdienst = ":????/services";
+ public static final String boards = ":11040/";
+ 
+ //USW...
+}
